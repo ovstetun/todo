375351b120bfc78a4956bab3aaa228d44358d3e2
Trond Marius Øvstetun <trond@ovstetun.no>
2012-04-19 11:08:42 +0200

    La til info for MS

--- todo.js
+++ todo.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var markdown = require("markdown-js");
+
 var app = express();
 var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render("index.jade");
 });
 
 app.listen(port, function() {
